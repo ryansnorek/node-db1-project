1167ac5f5fc982c06fd5faaa61671e27a069c358
Ryan <snorekr@gmail.com>
2021-12-06 15:55:44 -0500

    accounts model, update, delete

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -2,34 +2,34 @@ const db = require("../../data/db-config");
 
 const getAll = async () => {
   // SELECT * FROM accounts;
-  const accounts = await db("accounts")
-    .select("*");
+  const accounts = await db("accounts").select("*");
   return accounts;
 }
 
 const getById = async id => {
   // SELECT * FROM accounts WHERE id = id;
-  const account = await db("accounts")
-    .select("*")
-    .where("id", "=", id);
+  const account = await db("accounts").select("*").where("id", "=", id);
   return account;
 }
 
 const create = async newAccount => {
   // INSERT INTO accounts (name, budget) VALUES ('name', ####);
-  const [accountID] = await db("accounts")
-    .insert(newAccount);
+  const [accountID] = await db("accounts").insert(newAccount);
   const createdAccount = await getById(accountID);
   return createdAccount;
 }
 
-const updateById = (id, account) => {
-  
+const updateById = async (id, account) => {
+  // UPDATE accounts SET name = 'updated account' WHERE id = id;
+  await db("accounts").update(account).where("id", id);
+  const updatedAccount = await getById(id);
+  return updatedAccount;
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
-  console.log("connected")
+const deleteById = async id => {
+  // DELETE FROM accounts WHERE id = id;
+  await db("accounts").delete().where("id", id);
+  return `deleted ${id}`;
 }
 
 module.exports = {
